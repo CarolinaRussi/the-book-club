080f3e63994d338c698048750693057d459b051e
carolina <carolina.ferla@gran.com>
2025-10-30 10:30:40 -0300

    feat(frontend): create select customized with shadcn

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -5,6 +5,13 @@ import { useClub } from "../contexts/ClubContext";
 import { FiHome } from "react-icons/fi";
 import { TbBooks, TbCoffee } from "react-icons/tb";
 import { MdOutlineLogout, MdOutlinePeopleAlt } from "react-icons/md";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
 
 const privateNavItems = [
   { to: "/meetings", label: "Próximo Encontro", Icon: TbCoffee, size: 24 },
@@ -68,20 +75,33 @@ export default function Header() {
             ))}
 
             {userClubs.length > 1 && (
-              <select
-                value={selectedClubId || ""}
+              <Select
+                value={selectedClubId || undefined}
+                onValueChange={setSelectedClubId} // O shadcn já passa o (value: string)
                 disabled={isLoadingClubs}
-                onChange={handleChange}
-                className="items-center gap-2 px-4 py-2 rounded-md text-sm font-medium transition-all border-2 border-secondary bg-background text-foreground cursor-pointer"
               >
-                {isLoadingClubs && <option>Carregando clubes...</option>}
+                {/* Isso é o <select> (o botão que você vê)
+        Você pode centralizar o texto aqui! 
+      */}
+                <SelectTrigger className="w-[200px] border-2 border-secondary justify-center">
+                  <SelectValue placeholder="Carregando..." />
+                </SelectTrigger>
 
-                {userClubs.map((userClub) => (
-                  <option key={userClub.id} value={userClub.id}>
-                    {userClub.name}
-                  </option>
-                ))}
-              </select>
+                {/* Isso é o Menu de Opções
+        AQUI VOCÊ PODE ARREDONDAR! 
+      */}
+                <SelectContent className="border-secondary bg-background rounded-lg">
+                  {userClubs.map((userClub) => (
+                    <SelectItem
+                      key={userClub.id}
+                      value={userClub.id}
+                      className="cursor-pointer"
+                    >
+                      {userClub.name}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
             )}
 
             <button
