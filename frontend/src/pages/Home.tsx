4688c40a49f86196a4b865f2b59836cd8f743ffe
carolina <carolina.ferla@gran.com>
2025-10-30 11:34:22 -0300

    feat(fronted): create card with clubs on homepage

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,13 +1,14 @@
 import { FaPlus } from "react-icons/fa6";
-import { TbBooks } from "react-icons/tb";
 import { useAuth } from "../contexts/AuthContext";
 import { Link, useNavigate } from "react-router";
 import { MdOutlineEmail } from "react-icons/md";
+import { useClub } from "../contexts/ClubContext";
+import { Badge } from "../components/ui/badge";
 
 export default function Home() {
   const navigate = useNavigate();
   const { user } = useAuth();
-
+  const { userClubs } = useClub();
   // const handleLogin = () => {
   //   login("test-token", "test-user");
   //   navigate("/");
@@ -22,7 +23,7 @@ export default function Home() {
     <div className="bg-background flex flex-col items-center justify-start gap-4 min-h-screen p-20">
       <div className="flex flex-col w-full max-w-5xl">
         <div id="boas-vindas" className="flex flex-col items-start">
-          <h1 className="text-5xl mt-5 font-bold text-foreground ">
+          <h1 className="text-5xl font-bold text-foreground ">
             Olá, {user?.nickname || "Bem-vindo ao Clube do Livro"}
           </h1>
 
@@ -31,10 +32,10 @@ export default function Home() {
           </h3>
         </div>
 
-        <div className="grid grid-cols-2 gap-10 mt-5">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 lg:gap-10 mt-5">
           <div
             id="card-novo-clube"
-            className="border-2 text-foreground border-secondary rounded-lg p-6 w-full mt-5 shadow-md bg-background flex flex-col items-start justify-start gap-2 text-center"
+            className="border-2 text-foreground border-secondary rounded-lg p-6 w-full shadow-md bg-background flex flex-col items-start justify-start gap-2 text-center"
           >
             <div className="flex flex-row gap-2 items-center mb-2">
               <FaPlus size={20} className="text-primary" />
@@ -53,13 +54,12 @@ export default function Home() {
 
           <div
             id="card-biblioteca"
-            className="border-2 text-foreground border-secondary rounded-lg p-6 w-full mt-5 shadow-md bg-background flex flex-col items-start justify-start gap-2 text-center"
+            className="border-2 text-foreground border-secondary rounded-lg p-6 w-full shadow-md bg-background flex flex-col items-start justify-start gap-2 text-center"
           >
             <div className="flex flex-row gap-2 items-center mb-2">
               <MdOutlineEmail size={24} className="text-primary" />
               <h1 className="text-2xl font-semibold">Entrar em um clube</h1>
             </div>
-
             <h3 className="text-muted-foreground text-left">
               Recebeu um convite? Digite o código para entrar em um clube
             </h3>
@@ -79,11 +79,37 @@ export default function Home() {
             <h1 className="text-2xl ml-2 font-bold text-left text-foreground">
               Seus Clubes
             </h1>
-            <div className="items-center mt-3 justify-center text-warm-brown">
-              <h3>
-                Você ainda não faz parte de nenhum clube. Crie um ou use um
-                código de convite!
-              </h3>
+            <div className="items-center mt-1 justify-center text-warm-brown">
+              {userClubs.length > 0 ? (
+                <div>
+                  {userClubs.map((club) => (
+                    <div className="border-2 border-secondary rounded-lg p-5 w-full min-h-20 mt-5 shadow-md bg-cream flex flex-col gap-2 text-start">
+                      <div className="text-xl text-primary font-semibold">
+                        {club.name}
+                      </div>
+                      {club.description && (
+                        <div className="text-sm text-muted-foreground">
+                          {club.description}
+                        </div>
+                      )}
+                      <div className="flex flex-row gap-4 justify-between">
+                        <div className="text-xs text-muted-foreground">
+                          Criado em:{" "}
+                          {new Date(club.created_at).toLocaleDateString()}
+                        </div>
+                        <Badge className="bg-teal-700">
+                          {club.status == "active" ? "Ativo" : "Inativo"}
+                        </Badge>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              ) : (
+                <h3>
+                  Você ainda não faz parte de nenhum clube. Crie um ou use um
+                  código de convite!
+                </h3>
+              )}
             </div>
           </div>
         </div>
--- frontend/src/types/IClubs.ts
+++ frontend/src/types/IClubs.ts
@@ -1,6 +1,9 @@
 interface UserClub {
   id: string;
   name: string;
+  status: string;
+  created_at: string;
+  description?: string;
 }
 
 export type { UserClub };
