5a4d954fdbdcfb9869b4ea1972457e189cfbce97
carolina <carolina.ferla@gran.com>
2025-10-30 17:54:35 -0300

    feat(frontend): create badge for club owner

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -20,97 +20,102 @@ export default function Home() {
   };
 
   return (
-    <div className="bg-background flex flex-col items-center justify-start gap-4 min-h-screen p-20">
-      <div className="flex flex-col w-full max-w-5xl">
-        <div id="boas-vindas" className="flex flex-col items-start">
-          <h1 className="text-5xl font-bold text-foreground ">
-            Olá, {user?.nickname || "Bem-vindo ao Clube do Livro"}
-          </h1>
+    <div className="flex flex-col w-full max-w-5xl">
+      <div id="boas-vindas" className="flex flex-col items-start">
+        <h1 className="text-5xl font-bold text-foreground ">
+          Olá, {user?.nickname || "Bem-vindo ao Clube do Livro"}
+        </h1>
 
-          <h3 className="text-2xl mt-3 w-full text-muted-foreground">
-            O que você gostaria de fazer hoje?
+        <h3 className="text-2xl mt-3 w-full text-muted-foreground">
+          O que você gostaria de fazer hoje?
+        </h3>
+      </div>
+
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 lg:gap-10 mt-5">
+        <div
+          id="card-novo-clube"
+          className="border-2 text-foreground border-secondary rounded-lg p-6 w-full shadow-md bg-background flex flex-col items-start justify-start gap-2 text-center"
+        >
+          <div className="flex flex-row gap-2 items-center mb-2">
+            <FaPlus size={20} className="text-primary" />
+            <h1 className="text-2xl font-semibold">Criar novo clube</h1>
+          </div>
+          <h3 className="text-muted-foreground text-left">
+            Crie seu próprio clube do livro e convide amigos para participar
           </h3>
+          <button
+            onClick={() => openModalCreateBookClube()}
+            className="mt-4 font-semibold py-3 w-full rounded-xl bg-primary text-background cursor-pointer hover:bg-primary/80"
+          >
+            Criar clube
+          </button>
         </div>
 
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 lg:gap-10 mt-5">
-          <div
-            id="card-novo-clube"
-            className="border-2 text-foreground border-secondary rounded-lg p-6 w-full shadow-md bg-background flex flex-col items-start justify-start gap-2 text-center"
-          >
-            <div className="flex flex-row gap-2 items-center mb-2">
-              <FaPlus size={20} className="text-primary" />
-              <h1 className="text-2xl font-semibold">Criar novo clube</h1>
-            </div>
-            <h3 className="text-muted-foreground text-left">
-              Crie seu próprio clube do livro e convide amigos para participar
-            </h3>
-            <button
-              onClick={() => openModalCreateBookClube()}
-              className="mt-4 font-semibold py-3 w-full rounded-xl bg-primary text-background cursor-pointer hover:bg-primary/80"
-            >
-              Criar clube
-            </button>
+        <div
+          id="card-biblioteca"
+          className="border-2 text-foreground border-secondary rounded-lg p-6 w-full shadow-md bg-background flex flex-col items-start justify-start gap-2 text-center"
+        >
+          <div className="flex flex-row gap-2 items-center mb-2">
+            <MdOutlineEmail size={24} className="text-primary" />
+            <h1 className="text-2xl font-semibold">Entrar em um clube</h1>
           </div>
-
-          <div
-            id="card-biblioteca"
-            className="border-2 text-foreground border-secondary rounded-lg p-6 w-full shadow-md bg-background flex flex-col items-start justify-start gap-2 text-center"
+          <h3 className="text-muted-foreground text-left">
+            Recebeu um convite? Digite o código para entrar em um clube
+          </h3>
+          <button
+            onClick={() => openModalCreateBookClube()}
+            className="mt-4 font-semibold py-3 w-full rounded-xl bg-background border border-secondary shadow-md text-foreground hover:bg-cream hover:text-foreground cursor-pointer"
           >
-            <div className="flex flex-row gap-2 items-center mb-2">
-              <MdOutlineEmail size={24} className="text-primary" />
-              <h1 className="text-2xl font-semibold">Entrar em um clube</h1>
-            </div>
-            <h3 className="text-muted-foreground text-left">
-              Recebeu um convite? Digite o código para entrar em um clube
-            </h3>
-            <button
-              onClick={() => openModalCreateBookClube()}
-              className="mt-4 font-semibold py-3 w-full rounded-xl bg-primary text-background cursor-pointer hover:bg-primary/80"
-            >
-              Inserir código
-            </button>
-          </div>
+            Inserir código
+          </button>
         </div>
-        <div className="grid grid-cols-1 gap-10 mt-10">
-          <div
-            id="card-meus-clubes"
-            className="border-2 border-secondary rounded-lg p-7 w-full min-h-40 mt-3 shadow-md bg-cream flex flex-col gap-2 text-center"
-          >
-            <h1 className="text-2xl ml-2 font-bold text-left text-foreground">
-              Seus Clubes
-            </h1>
-            <div className="items-center mt-1 justify-center text-warm-brown">
-              {userClubs.length > 0 ? (
-                <div>
-                  {userClubs.map((club) => (
-                    <div className="border-2 border-secondary rounded-lg p-5 w-full min-h-20 mt-5 shadow-md bg-cream flex flex-col gap-2 text-start">
-                      <div className="text-xl text-primary font-semibold">
-                        {club.name}
-                      </div>
-                      {club.description && (
-                        <div className="text-sm text-muted-foreground">
-                          {club.description}
-                        </div>
-                      )}
-                      <div className="flex flex-row gap-4 justify-between">
-                        <div className="text-xs text-muted-foreground">
-                          Criado em:{" "}
-                          {new Date(club.created_at).toLocaleDateString()}
-                        </div>
-                        <Badge className="bg-teal-700">
-                          {club.status == "active" ? "Ativo" : "Inativo"}
+      </div>
+      <div className="grid grid-cols-1 gap-10 mt-10">
+        <div
+          id="card-meus-clubes"
+          className="border-2 border-secondary rounded-lg p-7 w-full min-h-40 mt-3 shadow-md bg-cream flex flex-col gap-2 text-center"
+        >
+          <h1 className="text-2xl ml-2 font-bold text-left text-warm-brown">
+            Seus Clubes
+          </h1>
+          <div className="items-center mt-1 justify-center text-warm-brown">
+            {userClubs.length > 0 ? (
+              <div>
+                {userClubs.map((club) => (
+                  <div className="border-2 border-secondary rounded-lg bg-background p-5 w-full min-h-20 mt-5 shadow-md flex flex-col gap-2 text-start">
+                    <div className="text-xl text-primary font-semibold">
+                      {club.name}
+                      {club.owner_id === user?.id ? (
+                        <Badge className="ml-2 py-1">Admin</Badge>
+                      ) : (
+                        <Badge className="ml-2 py-1 bg-warm-brown">
+                          Membro
                         </Badge>
-                      </div>
+                      )}
                     </div>
-                  ))}
-                </div>
-              ) : (
-                <h3>
-                  Você ainda não faz parte de nenhum clube. Crie um ou use um
-                  código de convite!
-                </h3>
-              )}
-            </div>
+                    {club.description && (
+                      <div className="text-sm text-muted-foreground">
+                        {club.description}
+                      </div>
+                    )}
+                    <div className="flex flex-row gap-4 justify-between">
+                      <div className="text-sm text-muted-foreground">
+                        Criado em:{" "}
+                        {new Date(club.created_at).toLocaleDateString()}
+                      </div>
+                      <Badge className="bg-teal-700">
+                        {club.status == "active" ? "Ativo" : "Inativo"}
+                      </Badge>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            ) : (
+              <h3>
+                Você ainda não faz parte de nenhum clube. Crie um ou use um
+                código de convite!
+              </h3>
+            )}
           </div>
         </div>
       </div>
--- frontend/src/types/IClubs.ts
+++ frontend/src/types/IClubs.ts
@@ -2,6 +2,7 @@ interface UserClub {
   id: string;
   name: string;
   status: string;
+  owner_id: string;
   created_at: string;
   description?: string;
 }
